dc45cf1056856c1df4d4823cf6ed0e01900d769f
Ian McNair <imcnair13@gmail.com>
2020-06-14 02:15:25 -0500

    Fixed bigram, added trigram features.

--- step2_Feature_Generation/.ipynb_checkpoints/sentence_to_feature-checkpoint.py
+++ step2_Feature_Generation/.ipynb_checkpoints/sentence_to_feature-checkpoint.py
@@ -6,6 +6,18 @@ Some features may require the entire dataset or additional information
 To Do
 [ ] Create a lemmatizer
 [ ] Figure out if cosine similarity is possible
+
+List of Working Features:
+- Sentence Manipulation
+    - Removing Stop words
+    - Porter Stem
+    - Alphabetical order
+- Count
+    - Word
+    - Sentence
+- Similarity Measures
+    - Jaccard
+    - Spacy
 """
 #Spacy Imports
 import spacy
@@ -58,7 +70,9 @@ def word_count(sentence):
 
 def sentence_count(sentence):
     return len(sentence.split("."))
-    
+
+################### similarity measures ####################
+
 def jaccard_similarity(student_answer, teacher_answer):
     a = set(student_answer.split(" "))
     b = set(teacher_answer.split(" "))
@@ -66,12 +80,19 @@ def jaccard_similarity(student_answer, teacher_answer):
     return (len(c) / (len(a) + len(b) - len(c)))
     
 def spacy_similarity(doc1, doc2):
+    """
+    Uses spacys built-in word embeddings to do similarity measures
+    May need more testing as its probably not ideal if this is good at matching words
+    and not phrases
+    
+    Is there a way I could match a phrase?
+    """
     student_answer = nlp(doc1)
     teacher_answer = nlp(doc2)
     return student_answer.similarity(teacher_answer)
 
-# Entity Extraction #
-def single_entity_extraction(df, sentence_col_name, answer):
+################### Entity Extraction ####################
+def unigram_entity_extraction(df, sentence_col_name, answer):
     """
     This breaks the sentence using spaces
     and then creates features based one each word
@@ -86,28 +107,60 @@ def single_entity_extraction(df, sentence_col_name, answer):
     return df
 
 def bigram_entity_extraction(df, sentence_col_name, answer):
-    # Create list of bigrams
+    """
+    This works, but doesn't havea high match rate.
+    More matches with the normal sentence, but it doesn't
+    correlate to prediction it seems
+    """
+    # Create list of bigrams for answer
     bigram_answer = create_list_of_bigrams(answer)
     
     # Need to compare bigrams to bigrams, below I am comparing
     # one word to bigram which is always going to return false.
     
     for bigram in bigram_answer:
-        df[f'has_{bigram}'] = df[sentence_col_name].apply(lambda sent: int(bigram in sent))
+        bigram_ = bigram.replace(" ", "_")
+        df[f'has_{bigram_}'] = df[sentence_col_name].apply(lambda sent: int(bigram in sent))
         
     return df
 
 def create_list_of_bigrams(sentence):
     sentence_list = sentence.split(" ")
-    bigram_list = [f"*_{sentence_list[0]}"]
-    
+    bigram_list = []
+
+    #For each word in sentence, but needed the index
     for i in range(len(sentence_list)):
-        
+        # For index out of bounds error prrevention
         if i < len(sentence_list)-1:
-            print(i)
-            bigram_list.append(f"{sentence_list[i]}_{sentence_list[i+1]}")
-        else:
-            print(i)
-            bigram_list.append(f"{sentence_list[i]}_*")
+            bigram_list.append(f"{sentence_list[i]} {sentence_list[i+1]}")
             
     return bigram_list
+
+def trigram_entity_extraction(df, sentence_col_name, answer):
+    trigram_answer = create_list_of_trigrams(answer)
+   
+    for trigram in trigram_answer:
+        trigram_ = trigram.replace(" ", "_")
+        df[f'has_{trigram_}'] = df[sentence_col_name].apply(lambda sent: int(trigram in sent))
+        
+    return df
+
+def create_list_of_trigrams(sentence):
+    sentence_list = sentence.split(" ")
+    trigram_list = []
+
+    #For each word in sentence, but needed the index
+    for i in range(len(sentence_list)):
+        # For index out of bounds error prrevention
+        if i < len(sentence_list)-2:
+            trigram_list.append(f"{sentence_list[i]} {sentence_list[i+1]} {sentence_list[i+2]}")
+            
+    return trigram_list
+
+################### Feature Reduction ####################
+def drop_column_if_all_same(df):
+    """
+    This scans each row and drops any column
+    """
+    df.loc[:, (df != 0).any(axis=0)]
+    return df
