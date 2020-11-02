fc8b3284a2332db347a60c23e64e0100df95c0ce
Ian McNair <imcnair13@gmail.com>
2020-11-01 20:40:53 -0600

    add files for heroku.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
 email = \"imcnair13@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+“ > ~/.streamlit/config.toml
